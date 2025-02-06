44c250e0be6eff54346f60909b4da0d2752e1692
Sayed Labib <98503095+SayedLabib@users.noreply.github.com>
2025-02-07 00:22:54 +0600

    News portal API integration

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+console.log('connected');
+
+const newsHeader = document.getElementById('News_Header');
+
+
+const p_tags = newsHeader.querySelectorAll('p');
+
+for(const p of p_tags )
+    {
+        p.classList.add
+        ('border-b-2', 
+        'border-transparent',
+        'hover:border-b-2',
+        'hover:border-b-[#5D5FEF]',
+        'transition',
+        'duration-200',
+        'cursor-pointer');
+    }
+
