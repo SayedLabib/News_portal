9a02de52d0ceaf4caa8c1267b59c6e7e1061e871
Sayed Labib <98503095+SayedLabib@users.noreply.github.com>
2025-02-08 19:38:05 +0600

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,48 @@
 
-console.log('connected');
 
-const newsHeader = document.getElementById('News_Header');
-
-
-const p_tags = newsHeader.querySelectorAll('p');
-
-for(const p of p_tags )
+const loadCategory = async() =>
     {
-        p.classList.add
-        ('border-b-2', 
-        'border-transparent',
-        'hover:border-b-2',
-        'hover:border-b-[#5D5FEF]',
-        'transition',
-        'duration-200',
-        'cursor-pointer');
+        const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+        const data = await res.json();
+        // console.log(data);   
+
+    
+        
+        const newsHeader = document.getElementById('News_Header');
+        
+        data.data.news_category.forEach((item)=>{
+           
+            // console.log(item);
+            
+            const pTags = document.createElement('p');
+
+         
+                    pTags.innerText = `${item.category_name}`;
+
+                    pTags.classList.add
+                    ('border-b-2', 
+                    'border-transparent',
+                    'hover:border-b-2',
+                    'hover:border-b-[#5D5FEF]',
+                    'transition',
+                    'duration-200',
+                    'cursor-pointer');
+             
+            
+           newsHeader.appendChild(pTags);
+
+        });
     }
+
+loadCategory();
+
+
+
+// News Header adding CSS properties
+
+
+
+
 
 
     const dropButton = document.getElementById('drop-button');
